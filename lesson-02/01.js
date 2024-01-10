81e114855ef86db0e1d3659a5232c94839a1d037
Dmitry Shchutsky <121363089+shuckiy73@users.noreply.github.com>
2024-01-10 22:26:40 +0300

    number1

--- lesson-02/01.js
+++ lesson-02/01.js
@@ -1,5 +1,6 @@
-let number = Math.floor(Math.random() * 100)
-console.log (nubmber++);
-console.log (++nubmber);
-console.log (nubmber--);
-console.log (--nubmber);
+let number1 = Math.floor(Math.random() * 100);// code
+if (number1 % 2 === 0) {
+    console.log(number1 + " :Это четное число");
+} else {
+    console.log(number1 + " :Это нечетное число");
+}
