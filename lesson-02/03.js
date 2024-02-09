10f17a4a7103b784b1ce1d906404c9378ceefc74
Dmitry Shchutsky <121363089+shuckiy73@users.noreply.github.com>
2024-02-09 20:33:10 +0300

    https://shuckiy73.github.io/pre-cource-hw-js/

--- lesson-02/03.js
+++ lesson-02/03.js
@@ -1,8 +1,8 @@
 const giveMeRightNumbers = (value1, value2, maximum) => {
-    if (value1 === value2 || value1 <= maximum) {
-        console.log('Success!');
-    } else {
-        console.log('Something wrong!');
-    }
-}
-giveMeRightNumbers(201, 100, 200)
+  if (value1 === value2 || value1 <= maximum) {
+    console.log("Success!");
+  } else {
+    console.log("Something wrong!");
+  }
+};
+https://shuckiy73.github.io/pre-cource-hw-js/(12, 10, 10);
